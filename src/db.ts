efafb335a43819d87837412c63f139e4d37311ca
ujwal <ujwalmogha2004@gmail.com>
2024-11-21 23:31:43 +0530

    Initial setup with db schema,userMiddleware and routes skeleton

---
+++ src/db.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const UserSchema = new Schema({
+    username : {type : String, required : true , unique : true},
+    password : {type : String , required : true}
+})
+
+const ContentSchema = new Schema({
+    link : { type : String, unique : true },
+    type : String,
+    title : String,
+    tag : [{type : Schema.Types.ObjectId , ref : "Tag"}],
+    userId : {type : Schema.Types.ObjectId , ref : "User" }, 
+})
+
+const LinkSchema = new Schema ({
+    hash : String,
+    userId : {type : Schema.Types.ObjectId , ref : "User" }, 
+})
+
+const TagSchema = new Schema ({
+    title : String,
+})
+
+const UserModel = mongoose.model("User" , UserSchema);
+const ContentModel = mongoose.model("Content" , ContentSchema);
+const LinkModel = mongoose.model("Link" , LinkSchema);
+const TagModel = mongoose.model("Tag" , TagSchema);
+
+export  {
+    UserModel,
+    ContentModel,
+    LinkModel,
+    TagModel
+}
---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from 'express';
+const JWT_SECRET = "UJWAL_SECRET";
+
+interface DecodeToken {
+    id : string
+} 
+
+interface RequestWithUser extends Request {
+    userId?: string;
+}
+
+
+function userMiddleware( req : RequestWithUser , res : Response , next : NextFunction) {
+    const token = req.headers.authorization;
+
+    if(!token){
+        return res.status(403).json({
+            message : "Token Does not exists"
+        })
+    }
+
+    try{
+         const decode = jwt.verify( token , JWT_SECRET) as DecodeToken;
+         if(!decode){
+            return res.status(401).json({
+                message : "Invalid Token"
+            })
+         }
+        
+        req.userId = decode.id;
+        next();
+    }
+    catch (error : any) {
+         return res.status(500).json({
+            message : "Internal server error",
+            error : error.message
+         })
+    }
+}
